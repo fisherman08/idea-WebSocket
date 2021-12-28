59eb82ac1255334286945c06e116b3cd5252ddce
kaneko <orenoikizama@hotmail.com>
2021-12-28 13:56:52 +0900

    publish

---
+++ src/main/kotlin/com/github/fisherman08/idea_websocket/platform/PluginIIcons.kt
@@ -0,0 +1,8 @@
+package com.github.fisherman08.idea_websocket.platform
+
+import com.intellij.openapi.util.IconLoader
+
+object PluginIIcons {
+    @JvmField
+    val WindowIcon = IconLoader.getIcon("/icons/pluginlogo_icon.svg", javaClass)
+}
